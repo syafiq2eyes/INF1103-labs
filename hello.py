1fd3782bd3a84dffb70a200599a8595b859b3f96
Syafiq <2603023@sit.singaporetech.edu.sg>
2026-09-04 15:42:03 +0800

    Added Assignments Variables

--- hello.py
+++ hello.py
@@ -18,13 +18,13 @@ print("Followers:", followers)
 
 followers = 100
 followers += 50
-print("Day 1: ",followers)
+print("Day 1:  ",followers)
 
 followers += 20
-print("Day 2: ",followers)
+print("Day 2:  ",followers)
 
 followers -= 10
-print("Day 3: ", followers)
+print("Day 3:  ", followers)
  
 
 #####Activity 4
