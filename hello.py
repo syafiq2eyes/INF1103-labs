8fee2df5516cfe608a7e4b9815801c27293fcf69
Syafiq <2603023@sit.singaporetech.edu.sg>
2026-09-04 15:33:29 +0800

    New Changes

--- hello.py
+++ hello.py
@@ -1,4 +1,59 @@
+#####Activity 1
 print("=====================")
 print("Welcome Here")
 print ("My First Post")
 print("=====================")
+
+#####Activity 2
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio", bio)
+print("Followers:", followers)
+
+#####Activity 3
+
+followers = 100
+followers += 50
+print("Day 1: ",followers)
+
+followers += 20
+print("Day 2: ",followers)
+
+followers -= 10
+print("Day 3: ", followers)
+ 
+
+#####Activity 4
+
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print ("\nInstagram Profile")
+print("===============")
+print("Username: " , username)
+print("Age: ", age)
+print("Category: ", category)
+
+#####Activity 5
+
+username = input("Enter Username: ")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+print ("\nInstagram Profile")
+print("===============")
+print("Username: " , username)
+print("Age: ", age)
+print("Category: ", category)
+
+if age>40 and category == "fun" or "Fun":
+    print("You are old. What is fun for you??")
+
+
+
+
