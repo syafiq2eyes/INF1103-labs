335b4b872fdc5e07aeb2e10c16023e70b3645193
Syafiq <2603023@sit.singaporetech.edu.sg>
2026-09-18 14:25:40 +0800

    Newly Week3 File Copied

---
+++ Syafiq_Week3/modular_auditor.py
@@ -0,0 +1,47 @@
+inventory = 0
+max_capacity = 1000
+failed_entries = 0
+total_units_processed = 0
+
+
+print("Inventory Auditor Started. Type 'quit' to exit." )
+
+while True:
+    entry = input("Enter Stock Quantity:")
+
+    if entry.lower() == "quit":
+        print("Final Inventory:", inventory)
+        print("Total Units Processed:", total_units_processed)
+        print("Failed/Rejected Entries:", failed_entries)
+        break
+
+    try:
+        quantity = int(entry)
+    except ValueError:
+        print("Error: Please enter a valid integer.")
+        failed_entries += 1
+        continue
+
+
+    if quantity < 0:
+        print("Error: Negative values are not allowed.")
+        failed_entries += 1
+        continue
+    if inventory + quantity > max_capacity:
+        print(f"Error: Adding {quantity} exceeds storage Capacity. {max_capacity}")
+        failed_entries += 1
+        continue
+
+    inventory += quantity
+    total_units_processed += quantity
+    print(f"stock added. Current inventory: {inventory}") 
+
+    if inventory > 500:
+        print("ALERT: Inventory exceeded 500 units. Stopping auditor immediately.")
+        print("\n--- Report ---")
+        print("Final Inventory:", inventory)
+        print("Total Units Processed:", total_units_processed)
+        print("Failed/Rejected Entries:", failed_entries)
+        break
+
+
