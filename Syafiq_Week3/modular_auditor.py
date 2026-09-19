9289185889c2844f187aa7711f46b3fc355bacf3
Syafiq <2603023@sit.singaporetech.edu.sg>
2026-09-19 16:57:05 +0800

    Added get_valid_input with errors

--- Syafiq_Week3/modular_auditor.py
+++ Syafiq_Week3/modular_auditor.py
@@ -1,39 +1,49 @@
-inventory = 0
 max_capacity = 1000
+tax_rate = 0.10
+
+inventory = 0
 failed_entries = 0
 total_units_processed = 0
-
-
-print("Inventory Auditor Started. Type 'quit' to exit." )
-
-while True:
-    entry = input("Enter Stock Quantity:")
+delivery_amount = 0
+ 
+def get_valid_input():
+    entry = input("Enter Stock Quantity: ")
 
     if entry.lower() == "quit":
         print("Final Inventory:", inventory)
         print("Total Units Processed:", total_units_processed)
         print("Failed/Rejected Entries:", failed_entries)
-        break
-
-    try:
-        quantity = int(entry)
-    except ValueError:
-        print("Error: Please enter a valid integer.")
-        failed_entries += 1
-        continue
-
+        
+        try:
+            quantity = int(entry)
+        except ValueError:
+            print("Error: Please enter a valid integer.")
+            failed_entries += 1
+            return None
 
     if quantity < 0:
         print("Error: Negative values are not allowed.")
         failed_entries += 1
-        continue
+        return None
+        
     if inventory + quantity > max_capacity:
         print(f"Error: Adding {quantity} exceeds storage Capacity. {max_capacity}")
         failed_entries += 1
-        continue
-
+        return None
+        
     inventory += quantity
     total_units_processed += quantity
+        
+        
+
+
+print("Inventory Auditor Started. Type 'quit' to exit." )
+
+while True:
+    
+    entry = get_valid_input()
+
+
     print(f"stock added. Current inventory: {inventory}") 
 
     if inventory > 500:
